4edd0af7591878ace5fa9128ea4d98cd62bb2b3d
Aidan Murray <aidanhansm@gmail.com>
2020-10-23 10:23:11 -0600

    Add model stuff for ride_mechanics

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,5 +1,6 @@
 class Mechanic < ApplicationRecord
   validates_presence_of :name, :experience
 
+  has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,5 +2,6 @@ class Ride < ApplicationRecord
   validates_presence_of :name, :thrill_rating
 
   belongs_to :park
-  has_many :mechanics, through: :ride_mechanics
+  has_many :ride_mechanics
+  has_many :mechanics, through: :ride_mechanics, source: :mechanic
 end
